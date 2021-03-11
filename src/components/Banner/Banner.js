1b2f8cff349394d42adfbd51cd2c834f801eb90c
Yeasin <yeasin30112001@gmail.com>
2021-03-10 20:33:34 -0600

    add some text in banner and some style

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -3,8 +3,12 @@ import './Banner.css'
 
 const Banner = () => {
     return (
-        <div className="banner">
-            <h1 className="text-white fw-bolder display-3 text-center">Sports Club</h1>
+        <div className="banner container-fluid">
+            <div className="col-md-6 text-white">
+                <h1 className=" fw-bolder display-4">Welcome To Sports Club</h1>
+                <p className="lead">Don't walk through life just playing football. Don't walk through life just being an athlete. Athletics will fade. Character and integrity and really making an impact on someone's life, that's the ultimate vision, that's the ultimate goal - bottom line</p>
+                <button className="btn btn-secondary main-btn">Learn More</button>
+            </div>
         </div>
     );
 };
